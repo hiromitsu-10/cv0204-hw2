081c10bf60cf0b193ed624b4f5aebb3b732cec63
kurokawa <tkuro1026@icloud.com>
2020-02-07 20:36:06 +0900

    追加しました

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react"
+import ReactDOM from "react-dom"
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+
+    const [count, setCount] = useState(0)
+
+    // 数字が増える
+    const upClick = () => {
+        setCount(count + 1)
+    }
+
+    // 数字が減る
+    const downClick = () => {
+        setCount(count - 1)
+    }
+
+    return (
+        <>
+            <h1>count:{count}</h1>
+
+            <button
+                type="button"
+                onClick={upClick}
+            >
+                増える
+            </button>
+
+            <button
+                type="button"
+                onClick={downClick}
+            >
+                減る
+            </button>
+        </>
+    )
+}
+
+ReactDOM.render(<App />,
+    document.getElementById("root")
+)
+
+
+
+
